c1e5fa3497d9005654ac915ec91a846ef239785a
Siddharth Jain <sidojain30705@gmail.com>
2024-09-21 20:00:54 +0530

    imporved security

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.not-found');
 const city = document.querySelector('.search-box input');
-const API_Key = '8c1d5802d34c8cd0f177ef09aca1fbe8';
+const API_Key = env_config.API_KEY;
 
 search.addEventListener('click', () => {
     funcMain();
